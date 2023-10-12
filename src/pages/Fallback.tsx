7d73a437b42b9f1f6c013e1d3e7c2a0d6086ec67
Sean Wong <seanwong24@live.com>
2023-10-12 15:05:41 +0000

    modify fallback page

--- src/pages/Fallback.tsx
+++ src/pages/Fallback.tsx
@@ -1,3 +1,4 @@
+import "@gaia/garage";
 import { useNavigate } from "@solidjs/router";
 import { langHelper } from "../utils/language";
 import { startViewTransition } from "../utils/startViewTransition";
@@ -5,16 +6,17 @@ import { startViewTransition } from "../utils/startViewTransition";
 function Fallback() {
   const navigate = useNavigate();
   return (
-    <div>
-      This Page is not ready yet.
+    <gaia-card class="absolute top-1/2 left-1/2 translate-[-50%] text-center">
+      <h3 slot="header">{langHelper("Fallback Page", "后备页面")}</h3>
+      {langHelper("This Page is not ready yet.", "这个页面还没有准备好。")}
       <br />
       <button
-        class="hue-rotating row-start-5 bg-blue-500 w-fit m-auto"
+        class="hue-rotating row-start-5 bg-blue-500 w-fit m-auto mt-3"
         onclick={() => startViewTransition(() => navigate("/"))}
       >
         {langHelper("Go Back Home", "回到主页")}
       </button>
-    </div>
+    </gaia-card>
   );
 }
 

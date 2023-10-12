650561ff0bc85d8a4ab8145e85c8637935176031
Sean Wong <seanwong24@live.com>
2023-10-12 14:42:49 +0000

    add fallback page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Router, Routes, Route } from "@solidjs/router";
 import { LanguageContext } from "./utils/language";
 import Home from "./pages/Home";
 import "./App.css";
+import Fallback from "./pages/Fallback";
 
 function App() {
   const [isEnglish, setIsEnglish] = createSignal(true);
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" component={Home} />
+          <Route path="/*" component={Fallback} />
         </Routes>
       </Router>
     </LanguageContext.Provider>
---
+++ src/pages/Fallback.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "@solidjs/router";
+import { langHelper } from "../utils/language";
+
+function Fallback() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      This Page is not ready yet.
+      <br />
+      <button
+        class="hue-rotating row-start-5 bg-blue-500 w-fit m-auto"
+        onclick={() => navigate("/")}
+      >
+        {langHelper("Go Back Home", "回到主页")}
+      </button>
+    </div>
+  );
+}
+
+export default Fallback;

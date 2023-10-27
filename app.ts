f4605787348f010312ac086fccf57bd0785f2b3e
Sean Wong <seanwong24@live.com>
2023-10-27 08:54:51 +0000

    update deps

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ import { startApp } from "deno-micro-cms";
 import { initDB } from "./init-db.ts";
 
 await startApp({
-  FE_USE_SPA: "TRUE",
   FE_ROOT_PATH: `${Deno.cwd()}/www/dist`,
-  DB_INIT: initDB
+  DB_INIT: initDB,
 });

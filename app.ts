04367c6b8e9f6e788fe10da45171550ee0b04317
Sean Wong <seanwong24@live.com>
2023-10-27 07:22:18 +0000

    add db initialization logic

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import { startApp } from "deno-micro-cms";
+import { initDB } from "./init-db.ts";
 
 await startApp({
   FE_USE_SPA: "TRUE",
   FE_ROOT_PATH: `${Deno.cwd()}/www/dist`,
+  DB_INIT: initDB
 });
---
+++ init-db.ts
@@ -0,0 +1,400 @@
+const data = {
+  footer: {
+    "address": {
+      "en": "Someplace, Somewhere, World",
+      "zh": "世界上某处某地",
+    },
+    "tel": {
+      "en": "+1(234)5678",
+      "zh": null,
+    },
+    "copyright": {
+      "en":
+        "© 2023 HISEN INTERNATIONAL CONSULTING LTD. All rights reserved | By Gaia Web Design",
+      "zh": null,
+    },
+  },
+  highlights: {
+    "badge": {
+      "en": "About Hisen",
+      "zh": "关于海盛",
+    },
+    "title": {
+      "en": "Hisen International Consulting Ltd.",
+      "zh": "海盛国际咨询有限公司",
+    },
+    "subtitle": {
+      "en":
+        "Immigration Specialists, who located in the downtown Vancouver, Canada, provide you with the most professional and caring service.",
+      "zh":
+        "定位于温哥华市中心的加拿大移民专家，为您提供最专业、最贴心的服务。",
+    },
+    "highlights": [
+      {
+        "icon": "/images/icon_placeholder.svg",
+        "baseColor": "#FEFCBF",
+        "title": {
+          "en": "Government certified",
+          "zh": "政府认证",
+        },
+        "content": {
+          "en":
+            "We are a Government of Canada approved service organization with membership in the Canadian Association of Immigration Consultants.",
+          "zh":
+            "我们是经过加拿大政府批准的服务机构，拥有加拿大移民顾问协会会员资格。",
+        },
+      },
+      {
+        "icon": "/images/icon_placeholder.svg",
+        "baseColor": "#C6F6D5",
+        "title": {
+          "en": "Deep Collaboration",
+          "zh": "深度合作",
+        },
+        "content": {
+          "en":
+            "In-depth partnerships have been established with a number of leading Canadian financial institutions and educational institutions.",
+          "zh": "与多家加拿大著名的金融机构、教育机构建立了深度合作关系。",
+        },
+      },
+      {
+        "icon": "/images/icon_placeholder.svg",
+        "baseColor": "#E9D8FD",
+        "title": {
+          "en": "Familiar with China's situation",
+          "zh": "熟悉中国国情",
+        },
+        "content": {
+          "en":
+            "We are well aware of China's policies and regulations and business management operations, and provide Chinese entrepreneurs with a full range of assistance and counseling.",
+          "zh":
+            "我们深知中国的政策法规与企业管理运营情况，为中国企业家提供全方位的帮助和咨询。",
+        },
+      },
+    ],
+  },
+  navItems: [
+    {
+      "title": {
+        "en": "Home",
+        "zh": "首页",
+      },
+      "href": "/",
+    },
+    {
+      "title": {
+        "en": "Immigration Entrances",
+        "zh": "移民通道",
+      },
+      "children": [
+        {
+          "title": {
+            "en":
+              "Intra-Company Transferee Work Permit / Owner Operator Work Permit",
+            "zh": "高管工签/投资人工签",
+          },
+          "href": "/entrances/ict-oowp",
+        },
+        {
+          "title": {
+            "en": "British Columbia Provincial Entrepreneur Immigration",
+            "zh": "BC投资移民类别",
+          },
+          "href": "entrepreneur-bcpnp",
+        },
+        {
+          "title": {
+            "en": "British Columbia Provincial Nominee Program",
+            "zh": "BC省提名项目",
+          },
+          "href": "bcpnp",
+        },
+        {
+          "title": {
+            "en": "Federal Express Entry Immigration Program",
+            "zh": "联邦类通道移民项目",
+          },
+          "href": "ee",
+        },
+        {
+          "title": {
+            "en": "Family Sponsorship Program",
+            "zh": "团聚移民项目",
+          },
+          "href": "sponsorship",
+        },
+      ],
+    },
+    {
+      "title": {
+        "en": "About Us",
+        "zh": "关于我们",
+      },
+      "href": "/about-us",
+    },
+    {
+      "title": {
+        "en": "News",
+        "zh": "新闻",
+      },
+      "href": "/news",
+    },
+    {
+      "title": {
+        "en": "EE Score Calculator",
+        "zh": "在线打分",
+      },
+      "href": "/ee-score-calculator",
+    },
+    {
+      "title": {
+        "en": "Contact Us",
+        "zh": "联系我们",
+      },
+      "href": "/contact-us",
+    },
+  ],
+  programs: {
+    "title": {
+      "part1": {
+        "en": "Canada Immigration Program",
+        "zh": "加拿大移民项目",
+      },
+      "part2": {
+        "en": " Glance",
+        "zh": "速览",
+      },
+    },
+    "subtitle": {
+      "en":
+        "Explore the various Canadian immigration pathways and find the one that suits you best. Whether you are a skilled worker, an entrepreneur or looking to reunite with your family, we offer detailed guidance and professional counseling services to help you realize your dream of moving to Canada.",
+      "zh":
+        "探索各种加拿大移民通道，找到最适合您的路径。无论您是技术工人、企业家还是希望与家人团聚，我们都为您提供详细的指导和专业的咨询服务，帮助您实现移居加拿大的梦想。",
+    },
+    "programs": [
+      {
+        "title": {
+          "en":
+            "Intra-Company Transferee Work Permit / Owner Operator Work Permit",
+          "zh": "高管工签 / 投资人工签",
+        },
+        "content": {
+          "en":
+            "For executives or investors with some domestic assets and management experience who wish to conduct business or invest in Canada.",
+          "zh":
+            "适用于在国内有一定资产和管理经验的高管或投资人，希望在加拿大开展业务或投资的人士。",
+        },
+        "applicationCycle": {
+          "en": "6-12 Months",
+          "zh": "6-12个月",
+        },
+        "image":
+          "https://images.pexels.com/photos/2916826/pexels-photo-2916826.jpeg?auto=compress&cs=tinysrgb&w=800",
+        "href": "/visa",
+      },
+      {
+        "title": {
+          "en": "Entrepreneur Immigration",
+          "zh": "BC投资移民类别",
+        },
+        "content": {
+          "en":
+            "The BC Immigrant Investor category is designed for foreign entrepreneurs who wish to invest, start a business and settle in B.C. for the long term. This program is designed to attract experienced business leaders who, through their knowledge and skills, will bring economic growth and new jobs to B.C.",
+          "zh":
+            "BC投资移民类别专为那些希望在BC省投资、创业并长期定居的外国企业家而设。此项目旨在吸引有经验的商业领袖，通过他们的知识和技能，为BC省带来经济增长和新的工作机会。",
+        },
+        "applicationCycle": {
+          "en":
+            "The application cycle is expected to take 14-20 months, depending on the completeness of the application materials and the speed of processing",
+          "zh":
+            "申请周期预计为14-20个月，具体时间取决于申请材料的完整性和处理速度",
+        },
+        "image":
+          "https://images.pexels.com/photos/2782485/pexels-photo-2782485.jpeg?auto=compress&cs=tinysrgb&w=800",
+        "href": "/bc-ei",
+      },
+      {
+        "title": {
+          "en": "British Columbia Provincial Nominee Program",
+          "zh": "BC省提名项目",
+        },
+        "content": {
+          "en":
+            "For skilled foreign workers, international students and entrepreneurs who wish to settle and work in B.C.",
+          "zh": "适用于希望在BC省定居并工作的外国技术工人、留学生和企业家。",
+        },
+        "applicationCycle": {
+          "en": "12-18 Months",
+          "zh": "12-18个月",
+        },
+        "image":
+          "https://images.pexels.com/photos/11424500/pexels-photo-11424500.jpeg?auto=compress&cs=tinysrgb&w=800",
+        "href": "/bc-pnp",
+      },
+      {
+        "title": {
+          "en": "Federal Express Entry Immigration Program",
+          "zh": "联邦类通道移民项目",
+        },
+        "content": {
+          "en":
+            "For skilled workers and professionals who wish to work or settle anywhere in Canada.",
+          "zh": "适用于希望在加拿大的任何地方工作或定居的技术工人和专业人士。",
+        },
+        "applicationCycle": {
+          "en": "12-24 Months",
+          "zh": "12-24个月",
+        },
+        "image":
+          "https://images.pexels.com/photos/7328/building-architecture-historical-tower.jpg?auto=compress&cs=tinysrgb&w=800",
+        "href": "/federal",
+      },
+      {
+        "title": {
+          "en": "Family Sponsorship Program",
+          "zh": "团聚移民项目",
+        },
+        "content": {
+          "en":
+            "Applies to foreign nationals who wish to be reunited with family members in Canada.",
+          "zh": "适用于希望与在加拿大的家人团聚的外国公民。",
+        },
+        "applicationCycle": {
+          "en": "18-36 Months",
+          "zh": "18-36个月",
+        },
+        "image":
+          "https://images.pexels.com/photos/2354073/pexels-photo-2354073.jpeg?auto=compress&cs=tinysrgb&w=800",
+        "href": "/family",
+      },
+    ],
+  },
+  slides: [
+    {
+      "title": {
+        "en": "Welcom to Hisen Inernational Consulting Ltd.",
+        "zh": "欢迎来到海盛国际咨询有限公司",
+      },
+      "subtitle": {
+        "en":
+          "Located in beautiful Vancouver, we provide professional Canadian immigration consulting services.",
+        "zh": "位于美丽的温哥华，为您提供专业的加拿大移民咨询服务。",
+      },
+      "image": "/images/slide_1.webp",
+    },
+    {
+      "title": {
+        "en": "Your Immigration Dreams, Our Commitment",
+        "zh": "您的移民梦，我们的承诺",
+      },
+      "subtitle": {
+        "en":
+          "An experienced team to guide you on the path to successful immigration.",
+        "zh": "经验丰富的团队，引导您走向成功的移民之路。",
+      },
+      "image": "/images/slide_2.webp",
+    },
+    {
+      "title": {
+        "en": "Canada, a new beginning",
+        "zh": "加拿大，一个新的开始",
+      },
+      "subtitle": {
+        "en": "Explore this land of opportunity and natural beauty.",
+        "zh": "探索这片充满机会和自然之美的土地。",
+      },
+      "image": "/images/slide_3.webp",
+    },
+  ],
+  testimonials: {
+    "title": {
+      "en": "Testimonials",
+      "zh": "客户心声",
+    },
+    "subtitle": {
+      "en": "Trusted and appreciated by customers worldwide",
+      "zh": "深受全球客户的信赖与好评",
+    },
+    "testimonials": [
+      {
+        "title": {
+          "en": "Professional and efficient",
+          "zh": "专业且高效",
+        },
+        "content": {
+          "en":
+            "Working with Hisen was one of the smartest choices I have ever made. Their team is very professional and provided me with a lot of valuable advice.",
+          "zh":
+            "与海盛合作是我做过的最明智的选择之一。他们的团队非常专业，为我提供了很多有价值的建议。",
+        },
+        "avatar":
+          "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+        "customerName": {
+          "en": "Mr. Lee",
+          "zh": "李先生",
+        },
+        "customerTitle": {
+          "en": "IT Project Manager - BCPNP Immigration Applicant",
+          "zh": "IT 项目经理 - BCPNP移民申请者",
+        },
+      },
+      {
+        "title": {
+          "en": "Meticulous service",
+          "zh": "服务细致入微",
+        },
+        "content": {
+          "en":
+            "The team at Hisen was really attentive, they helped me plan the whole immigration process and made me feel at ease.",
+          "zh":
+            "Hisen的团队真的很细心，他们帮我规划了整个移民过程，让我感到非常放心。",
+        },
+        "avatar":
+          "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+        "customerName": {
+          "en": "Ms. Wang",
+          "zh": "王女士",
+        },
+        "customerTitle": {
+          "en": "Marketing Manager - Federal Immigration Applicants",
+          "zh": "市场营销经理 - 联邦移民申请者",
+        },
+      },
+      {
+        "title": {
+          "en": "Stunning service",
+          "zh": "令人震撼的服务",
+        },
+        "content": {
+          "en":
+            "My friend recommended me to use Hisen and now I would like to recommend them to others. They are really trustworthy..",
+          "zh":
+            "我的朋友推荐我使用Hisen，现在我也愿意推荐给其他人。他们真的很值得信赖。",
+        },
+        "avatar":
+          "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80",
+        "customerName": {
+          "en": "Mr. Chang",
+          "zh": "张先生",
+        },
+        "customerTitle": {
+          "en": "Entrepreneur",
+          "zh": "企业家",
+        },
+      },
+    ],
+  },
+};
+
+export async function initDB(db: Deno.Kv) {
+  const prefix = ["app", "general"];
+  const entries = Object.entries(data).map(([key, value]) => ({
+    key: prefix.concat(key),
+    value,
+  }));
+  for (const { key, value } of entries) {
+    if (!(await db.get(key)).value) {
+      await db.set(key, value);
+    }
+  }
+}
